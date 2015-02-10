b74571eb62b627c7ef5818e8138869386658138e
Bret Comnes <bcomnes@gmail.com>
2015-02-09 16:48:21 -0800

    working though group lookup

--- listgen/listgen.py
+++ listgen/listgen.py
@@ -3,23 +3,31 @@ import grp
 
 
 def listfulldir(d):
-    return [os.path.join(d, f) for f in os.listdir(path=d)]
+    return [os.path.join(d, f) + '/' for f in os.listdir(path=d)]
 
 
 def group_lookup(path):
-    group = grp.getgrgid(os.stat(path).st_gid)
+    print(path)
+    gid = os.stat(path).st_gid
+    print(gid)
+    group = grp.getgrgid(gid)
     return group
 
-
 def get_resgroups(paths):
-    return [group_lookup for path in paths]
+    return [group_lookup(path) for path in paths]
 
+def members_lookup(path):
+    members = grp.getgrgid(os.stat(path).st_gid).gr_mem
+
+def get_members(paths):
+    return [members_lookup for path in paths]
 
 def www_lookup():
+    print('hi')
     www_path = '/vol/www/'
-    www_dirs = os.listdir(www_path)
+    www_dirs = listfulldir(www_path)
     www_resgroups = get_resgroups(www_dirs)
-    return www_resgroups
+    return "list of users"
 
 
 groups = {
