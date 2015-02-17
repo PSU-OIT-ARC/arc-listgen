bd76c3262a3869d54015884767b8fc922617b85b
Bret Comnes <bcomnes@gmail.com>
2015-02-16 16:28:29 -0800

    pep8 formatting

--- listgen/listgen.py
+++ listgen/listgen.py
@@ -1,36 +1,38 @@
 import os
 import grp
-import re
-import random
 import ldap3
 
 #user_re = re.compile(r'(pdx){1}(\d{5}){1}$')
 
-ldap_server = ldap3.Server('ldap-login.oit.pdx.edu', tls=None)
-connection = ldap3.Connection(ldap_server, auto_bind=True, lazy=True)
+SERVER = ldap3.Server('ldap-login.oit.pdx.edu', tls=None)
+CONNETION = ldap3.Connection(SERVER, auto_bind=True, lazy=True)
+
 
 def ldap_lookup(query):
-    with connection:
-        result = connection.search(
+    with CONNETION:
+        result = CONNETION.search(
             attributes=ldap3.ALL_ATTRIBUTES,
             search_base="dc=pdx,dc=edu",
             search_filter=query,
             search_scope=ldap3.SEARCH_SCOPE_WHOLE_SUBTREE,
-            )
+        )
     if result:
-        return connection.response
+        return CONNETION.response
     return []
+
 
 def listfulldir(d):
     return [os.path.join(d, f) + '/' for f in os.listdir(path=d)]
 
-exclude = {'other', 'root','sys','operator'}
+EXCLUDE = {'other', 'root', 'sys', 'operator'}
+
 
 def www_lookup():
     www_path = '/vol/www/'
     www_dirs = listfulldir(www_path)
     www_resgroups = get_resgroups(www_dirs)
     return get_members(www_resgroups)
+
 
 def group_lookup(path):
     try:
@@ -40,10 +42,12 @@ def group_lookup(path):
     group = grp.getgrgid(gid).gr_name
     return group
 
+
 def get_resgroups(paths):
     exclude = {'other', 'root'}
     resgroups = {group_lookup(path) for path in paths if group_lookup(path)}
     return resgroups - exclude
+
 
 def get_members(resgroups):
     www_users = set()
@@ -52,15 +56,14 @@ def get_members(resgroups):
             .get('attributes').get('memberUid')
         if members:
             www_users = www_users | set(members)
-    return www_users - exclude
-
-#######
+    return www_users - EXCLUDE
 
 
 def www_grplookup():
     www_path = '/vol/www/'
     www_dirs = listfulldir(www_path)
     return get_grpmembers(www_dirs)
+
 
 def grpmembers_lookup(path):
     try:
@@ -69,21 +72,20 @@ def grpmembers_lookup(path):
         return
     return grp.getgrgid(gid).gr_mem
 
+
 def get_grpmembers(paths):
     www_users = set()
     for path in paths:
         members = grpmembers_lookup(path)
         if members:
             www_users = www_users | set(members)
-    return www_users - exclude
+    return www_users - EXCLUDE
 
-####
-#
 
 def compare():
     return www_lookup() - www_grplookup()
 
-groups = {
+GROUPS = {
     'www': www_lookup,
     'wwwgrp': www_grplookup,
     'compare': compare,
@@ -92,5 +94,5 @@ groups = {
 
 
 def cli(group):
-    grouplist = groups.get(group, 'invalid group name')
+    grouplist = GROUPS.get(group, 'invalid group name')
     print(grouplist())
